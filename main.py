3a5bb64ba29aef377f9713c2bbffc9eee1e79d1b
neeltron <nprosofficial@gmail.com>
2023-10-07 22:47:32 +0000

    Added functionality to receive files from client app

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/upload', methods = ["POST"])
+def upload():
+  if request.method == 'POST':
+    file = request.files['file']
+    print("req received")
+    if file:
+      print("file is there")
+      file.save("audio.m4a")
+      return "upload successful!"
+    else:
+      return "no file"
+  
 
 app.run(host='0.0.0.0', port=8080)
