bb5b24f667892f34c37be92d627a83c5ae6cbdc6
neeltron <nprosofficial@gmail.com>
2023-10-08 00:06:50 +0000

    Added Google Cloud Speech Boilerplate

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 from flask import Flask, request
+import os
+from google.cloud import speech
+
+credential_path = "bruh.json"
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credential_path
+
 app = Flask('app')
+
+def transcribe_file(speech_file: str) -> speech.RecognizeResponse:
+    client = speech.SpeechClient()
+
+    with open(speech_file, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        sample_rate_hertz=8000,
+        language_code="en-US",
+    )
+
+    response = client.recognize(config=config, audio=audio)
+    print(response)
+    print("here")
+    for result in response.results:
+        print("here2")
+        print(result)
+
+    return response
+
+print(transcribe_file("download.mp3"))
 
 @app.route('/')
 def hello_world():
